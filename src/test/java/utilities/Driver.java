f1f5e0ebbd810db96fc07617e34d041e44ab9334
Beyza Nergiz <byz.nergiz34@gmail.com>
2024-01-21 14:11:38 +0300

    21/01/2024 Nergiz ilk ve son push:)

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,79 +1,68 @@
 package utilities;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class Driver {
 
-    public static WebDriver driver;
-
-    /*
-     * This method will create a driver and return it
-     * @return WebDriver driver
+    /* JUnit'de WebDriver objesi TestBaas'den geliyordu.
+    TestNG extends ile bağlanma zorunluluğunu otrtadan kaldirmak
+    ve testi yazanlara daha fazla kontrol imkani vermek icin
+    TestBase yerine Driver class'inda static 2 method ile
+    driver olusturma ve kapatma islemlerini yapmayi tercih etmistir
      */
+    static WebDriver driver;
+
     public static WebDriver getDriver() {
 
-        String browser = ConfigReader.getProperty("browser");
-        switch (browser) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "chrome-headless":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "firefox-headless":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
-                break;
-            case "ie":
-                if (!System.getProperty("os.name").toLowerCase().contains("windows"))
-                    throw new WebDriverException("Your OS doesn't support Internet Explorer");
-                WebDriverManager.iedriver().setup();
-                driver = new InternetExplorerDriver();
-                break;
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            case "safari":
-                if (!System.getProperty("os.name").toLowerCase().contains("mac"))
-                    throw new WebDriverException("Your OS doesn't support Safari");
-                WebDriverManager.getInstance(SafariDriver.class).setup();
-                driver = new SafariDriver();
-                break;
+        if (driver == null) {
+
+            String browser = ConfigReader.getProperty("browser");
+
+            switch (browser) {
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         }
 
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(ConfigReader.getProperty("kiwiUrl"));
 
         return driver;
-    }
-    /*
-     * This method quits the browser
-     */
-    public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
+}
+
+    public static void closeDriver(){
+        if (driver != null){
+            driver.close();
+            driver=null;
         }
     }
 
+    public static void quitDriver(){
+        if (driver != null){
+            driver.quit();
+            driver=null;
+        }
+    }
 }
---
+++ src/test/resources/features/US_05.feature
@@ -0,0 +1,10 @@
+@Kiwi
+Feature: Social media icons
+
+    Scenario: Social media icons testing
+
+      * Open the browser and go to "kiwiUrl"
+      * Accept the cookies
+      * It is tested whether "Instagram", "Twitter", "Linkedin", "Facebook" in the Footer section are displayed.
+      * It is tested whether "Instagram", "Twitter", "Linkedin", "Facebook" in the Footer section are active.
+      * Close the browser
