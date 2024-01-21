a52d4ba4cc10b35b042fb1c23f0452406f0b6b00
AysegulHayal <i.hayal3461@icloud.com>
2024-01-22 01:53:58 +0300

    22.01.2024 ilk ama not son

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,6 +1,16 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class HomePage {
 
 
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    public void goTokiwiUrl(String url){
+        Driver.getDriver().get(ConfigReader.getProperty(url));
+    }
 }
---
+++ src/test/resources/features/US_03.feature
@@ -0,0 +1,7 @@
+
+Feature: Testing kiwi
+
+  Scenario:
+       * Open the browser and go to "kiwiUrl"
+       * Verify that I have reached the Kiwi home page by logging in
+       * Verify whether a new account can be opened.
